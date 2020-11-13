314653fb4da66e9a1dcada09690ab834d1ee49fb
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-11-13 17:41:31 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <p data-ns-test="project-title">Tv Series Tracker Application.</p>
+      <p data-ns-test="project-description">
+        Track progress of tv series of different platforms at a single place.
+      </p>
+    </>
+  );
+};
 
 export default App;
